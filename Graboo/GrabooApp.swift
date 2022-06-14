bf71d1d4f31c174f18a6c0052b8f2f355d57abc4
James Shiffer <2191476+scoliono@users.noreply.github.com>
2022-06-13 20:49:20 -0700

    Initial Commit

---
+++ Graboo/GrabooApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrabooApp.swift
+//  Graboo
+//
+//  Created by James Shiffer on 6/13/22.
+//
+
+import SwiftUI
+
+@main
+struct GrabooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
